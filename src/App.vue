24bff947f3d322b6578858c6f8efd1eed41395b3
Elias notebook <eliasbragga44@gmail.com>
2022-05-23 17:05:48 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+ 
+    <TelaPrincipal/>
+  </div>
+    
+   
+ 
+</template>
+
+<script>
+import TelaPrincipal from './components/TelaPrincipal.vue';
+
+
+
+export default {
+  name: 'App',
+
+  components: {
+    TelaPrincipal
+},
+
+  data(){
+    return{     
+
+    }
+    
+  },
+  methods:{
+    
+  }
+};
+</script>
+
+<style>
+#tela{
+  color: rgb(31, 31, 31);
+}
+
+
+.theme--light.v-btn.v-btn--has-bg {
+   
+}
+
+.v-btn:not(.v-btn--outlined).success, .v-btn:not(.v-btn--outlined).error, .v-btn:not(.v-btn--outlined).warning, .v-btn:not(.v-btn--outlined).info {
+
+}
+
+
+.theme--dark.v-sheet {
+   
+    
+    
+}
+
+.v-icon.v-icon{
+  /* margin-right: 20px; */
+}
+</style>
---
+++ src/components/TelaPrincipal.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="tela">
+    <h1>Ola Mundo</h1>
+    <input v-model="nota" type="text" id="texto-nota" />
+    <h1>{{ res }}</h1>
+    <v-btn @click="alerta()" flat slot="Ativar" id="button" class="success"
+      >Ativar</v-btn
+    >
+
+    <v-alert v-show="mostrar" shaped prominent type="error">
+        Digite apenas notas
+    </v-alert>
+    <v-btn href="TelaPrincipal" @click="tentarNovamente" v-show="buttonOff" flat slot="Ativar" id="button" class="success"
+      >Tentar novamente</v-btn
+    >   
+
+       <v-alert
+      shaped
+      prominent
+      type="error"
+    >
+      I'm a shaped alert with a dense prominent option that Praesent blandit laoreet nibh. Praesent nonummy mi in odio. Phasellus tempus. Mauris turpis nunc, blandit et, volutpat molestie, porta ut, ligula. Duis leo.
+    </v-alert>
+
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TelaPrincipal",
+  data() {
+    return {
+      buttonOff:false,  
+      mostrar: false,
+      nota: "",
+      res: this.nota,
+    };
+  },
+  methods: {
+    alerta() {
+      if (this.nota == "C") {
+        this.res = this.nota;
+      } else {
+        this.mostrar = true;
+        this.buttonOff=!this.buttonOff
+        
+      }
+
+      // this.mostrar = !this.mostrar
+    },
+    // tentarNovamente(){
+    //     this.mostrar = false
+    //     this.res = ""
+    //     this.nota=""
+    //     this.buttonOff=false
+        
+    // }
+
+  },
+};
+</script>
+
+
+<style>
+</style>
